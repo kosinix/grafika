e7ae6f49435839f3fd6895752848646ac87df05c
kosinix <kosinix@users.noreply.github.com>
2016-07-28 11:18:21 +0800

    Add Imagick pixelate filter

--- src/Grafika/Grafika.php
+++ src/Grafika/Grafika.php
@@ -35,6 +35,7 @@ use Grafika\Imagick\Filter\Gamma as ImagickGamma;
 use Grafika\Imagick\Filter\Dither as ImagickDither;
 use Grafika\Imagick\Filter\Grayscale as ImagickGrayscale;
 use Grafika\Imagick\Filter\Invert as ImagickInvert;
+use Grafika\Imagick\Filter\Pixelate as ImagickPixelate;
 use Grafika\Imagick\Filter\Sobel as ImagickSobel;
 use Grafika\Imagick\Image as ImagickImage;
 
@@ -208,6 +209,10 @@ class Grafika
                     return new ImagickGrayscale();
                 case 'Invert':
                     return new ImagickInvert();
+                case 'Pixelate':
+                    return new ImagickPixelate(
+                        $p[1]
+                    );
                 case 'Sobel':
                     return new ImagickSobel();
             }
---
+++ src/Grafika/Imagick/Filter/Pixelate.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Grafika\Imagick\Filter;
+
+use Grafika\FilterInterface;
+use Grafika\Imagick\Image;
+
+/**
+ * Pixelate an image
+ */
+class Pixelate implements FilterInterface{
+
+    /**
+     * @var int $amount Pixelate size from >= 1
+     */
+    protected $amount;
+
+    public function __construct($amount)
+    {
+        $this->amount = (int) $amount;
+    }
+
+    /**
+     * @param Image $image
+     *
+     * @return Image
+     */
+    public function apply( $image ) {
+
+        $size = $this->amount;
+        $width = $image->getWidth();
+        $height = $image->getHeight();
+        $image->getCore()->scaleImage(max(1, ($width / $size)), max(1, ($height / $size)));
+        $image->getCore()->scaleImage($width, $height);
+        return $image;
+    }
+
+}
