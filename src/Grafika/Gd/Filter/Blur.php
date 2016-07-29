75275fe641c5304944f23b153ba65cdf295af67d
kosinix <kosinix@users.noreply.github.com>
2016-07-29 11:47:17 +0800

    Update Gd blur phpdoc

--- src/Grafika/Gd/Filter/Blur.php
+++ src/Grafika/Gd/Filter/Blur.php
@@ -6,13 +6,20 @@ use Grafika\FilterInterface;
 use Grafika\Gd\Image;
 
 /**
- * Blurs the image
+ * Blurs the image.
  */
 class Blur implements FilterInterface
 {
 
+    /**
+     * @var int
+     */
     protected $amount;
 
+    /**
+     * Blur constructor.
+     * @param int $amount The amount of blur to apply. Possible values 1-100.
+     */
     public function __construct($amount = 1)
     {
         $this->amount = (int) $amount;
