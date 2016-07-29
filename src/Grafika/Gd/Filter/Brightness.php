8458291462d3333e8b6df9fa169c775202324fb1
kosinix <kosinix@users.noreply.github.com>
2016-07-29 12:10:43 +0800

    Update Gd brightness phpdoc

--- src/Grafika/Gd/Filter/Brightness.php
+++ src/Grafika/Gd/Filter/Brightness.php
@@ -6,12 +6,21 @@ use Grafika\FilterInterface;
 use Grafika\Gd\Image;
 
 /**
- * Changes the brightness TODO: param checks
+ * Change the image brightness.
+ *
+ * TODO: param checks
  */
 class Brightness implements FilterInterface{
 
+    /**
+     * @var int
+     */
     protected $amount; // -100 >= 0 >= 100
 
+    /**
+     * Brightness constructor.
+     * @param int $amount The amount of brightness to apply. >= -100 and <= -1 to darken. 0 for no change. >= 1 and <= 100 to brighten.
+     */
     public function __construct($amount)
     {
         $this->amount = (int) $amount;
