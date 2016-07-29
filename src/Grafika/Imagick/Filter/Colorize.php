55f1f354a04d0e59f939c7c9330739f14a1a90be
kosinix <kosinix@users.noreply.github.com>
2016-07-29 12:19:18 +0800

    Update Imagick colorize phpdoc

--- src/Grafika/Imagick/Filter/Colorize.php
+++ src/Grafika/Imagick/Filter/Colorize.php
@@ -6,14 +6,29 @@ use Grafika\FilterInterface;
 use Grafika\Imagick\Image;
 
 /**
- * Change the values for red, green and blue.
+ * Change the values for red, green and blue in an image.
  */
 class Colorize implements FilterInterface{
 
+    /**
+     * @var int
+     */
     protected $red; // -100 >= 0 >= 100
+    /**
+     * @var int
+     */
     protected $green; // -100 >= 0 >= 100
+    /**
+     * @var int
+     */
     protected $blue; // -100 >= 0 >= 100
 
+    /**
+     * Colorize constructor.
+     * @param int $red The amount of red colors. >= -100 and <= -1 to reduce. 0 for no change. >= 1 and <= 100 to add.
+     * @param int $green The amount of green colors. >= -100 and <= -1 to reduce. 0 for no change. >= 1 and <= 100 to add.
+     * @param int $blue The amount of blue colors. >= -100 and <= -1 to reduce. 0 for no change. >= 1 and <= 100 to add.
+     */
     public function __construct($red, $green, $blue)
     {
         $this->red = intval($red);
