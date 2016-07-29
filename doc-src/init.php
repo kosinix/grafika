05ffe41f82a51bdcc36fe044ad03d45fd4c74414
kosinix <kosinix@users.noreply.github.com>
2016-07-29 11:43:42 +0800

    Add doc-src helper for getting class description

--- doc-src/init.php
+++ doc-src/init.php
@@ -11,3 +11,9 @@ $doc_src = '';
 if(false !== $pos ){
     $doc_src = substr($haystack, 0, $pos+strlen($needle));
 }
+
+// Bandaid helper. class doc should be one liner only or this breaks.
+function classDoc($classPath){
+    $class = new ReflectionClass($classPath);
+    return preg_replace(array('(\/\*\*[\s]+[*][\s])','([\s]\*\/)'), '', $class->getDocComment());
+}
