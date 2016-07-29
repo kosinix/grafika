297bf53100f81d7f7e8a1ea54fb8527efc0dbdbd
kosinix <kosinix@users.noreply.github.com>
2016-07-29 11:47:29 +0800

    Update Imagick blur phpdoc

--- src/Grafika/Imagick/Filter/Blur.php
+++ src/Grafika/Imagick/Filter/Blur.php
@@ -6,12 +6,19 @@ use Grafika\FilterInterface;
 use Grafika\Imagick\Image;
 
 /**
- * Blurs the image
+ * Blurs the image.
  */
 class Blur implements FilterInterface{
 
+    /**
+     * @var int
+     */
     protected $amount;
 
+    /**
+     * Blur constructor.
+     * @param int $amount The amount of blur to apply. Possible values 1-100.
+     */
     public function __construct($amount = 1)
     {
         $this->amount = (int) $amount;
