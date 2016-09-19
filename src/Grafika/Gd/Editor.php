16ec1875c7ad716b00266801331c2640df766c2c
kosinix <kosinix@users.noreply.github.com>
2016-09-19 10:49:05 +0800

    Clone of image if needed

--- src/Grafika/Gd/Editor.php
+++ src/Grafika/Gd/Editor.php
@@ -54,11 +54,15 @@ final class Editor implements EditorInterface
         if (is_string($image1)) { // If string passed, turn it into a Image object
             $image1 = Image::createFromFile($image1);
             $image1->flatten();
+        } else {
+            $image1 = clone $image1; // Make sure we are working on the clone if Image is passed
         }
 
         if (is_string($image2)) { // If string passed, turn it into a Image object
             $image2 = Image::createFromFile($image2);
             $image2->flatten();
+        } else {
+            $image2 = clone $image2; // Make sure we are working on the clone if Image is passed
         }
 
         $hash = new DifferenceHash();
