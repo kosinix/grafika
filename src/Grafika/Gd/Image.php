5ed2e32e49ac3374863cb951c81151c6adb482fb
kosinix <kosinix@users.noreply.github.com>
2016-09-23 10:28:02 +0800

    Reformat and add description

--- src/Grafika/Gd/Image.php
+++ src/Grafika/Gd/Image.php
@@ -82,7 +82,7 @@ final class Image implements ImageInterface {
 
     /**
      * Output a binary raw dump of an image in a specified format.
-     * 
+     *
      * @param string|ImageType $type Image format of the dump.
      *
      * @throws \Exception When unsupported type.
@@ -111,35 +111,37 @@ final class Image implements ImageInterface {
     }
 
     /**
-     * @param $imageFile
+     * Create Image from image file.
+     *
+     * @param string $imageFile Path to image.
      *
      * @return Image
      * @throws \Exception
      */
-    public static function createFromFile( $imageFile ){
+    public static function createFromFile( $imageFile ) {
         if ( ! file_exists( $imageFile ) ) {
-            throw new \Exception( sprintf('Could not open "%s". File does not exist.', $imageFile) );
+            throw new \Exception( sprintf( 'Could not open "%s". File does not exist.', $imageFile ) );
         }
 
-        $type = self::_guessType($imageFile);
-        if ( ImageType::GIF == $type) {
+        $type = self::_guessType( $imageFile );
+        if ( ImageType::GIF == $type ) {
 
-            return self::_createGif($imageFile);
+            return self::_createGif( $imageFile );
 
-        } else if ( ImageType::JPEG == $type) {
+        } else if ( ImageType::JPEG == $type ) {
 
-            return self::_createJpeg($imageFile);
+            return self::_createJpeg( $imageFile );
 
-        } else if ( ImageType::PNG == $type) {
+        } else if ( ImageType::PNG == $type ) {
 
-            return self::_createPng($imageFile);
+            return self::_createPng( $imageFile );
 
-        } else if ( ImageType::WBMP == $type) {
+        } else if ( ImageType::WBMP == $type ) {
 
-            return self::_createWbmp($imageFile);
+            return self::_createWbmp( $imageFile );
 
         } else {
-            throw new \Exception( sprintf('Could not open "%s". File type not supported.', $imageFile) );
+            throw new \Exception( sprintf( 'Could not open "%s". File type not supported.', $imageFile ) );
         }
     }
 
