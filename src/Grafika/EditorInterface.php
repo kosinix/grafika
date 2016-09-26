cf99e9fd023983ac6c43a337a91ea1514f09cfa9
kosinix <kosinix@users.noreply.github.com>
2016-09-26 10:48:57 +0800

    Change contract for blend to use primitive types as parameters

--- src/Grafika/EditorInterface.php
+++ src/Grafika/EditorInterface.php
@@ -23,12 +23,14 @@ interface EditorInterface {
      * @param ImageInterface $image1 The base image.
      * @param ImageInterface $image2 The image placed on top of the base image.
      * @param string $type The blend mode. Can be: normal, multiply, overlay or screen.
-     * @param Position $position The position of $image2 on $image1.
-     * @param int $opacity The opacity of $image2. Possible values 0.0 to 1.0.
+     * @param float $opacity The opacity of $image2. Possible values 0.0 to 1.0 where 0.0 is fully transparent and 1.0 is fully opaque. Defaults to 1.0.
+     * @param string $position The position of $image2 on $image1. Possible values top-left, top-center, top-right, center-left, center, center-right, bottom-left, bottom-center, bottom-right and smart. Defaults to top-left.
+     * @param int $offsetX Number of pixels to add to the X position of $image2.
+     * @param int $offsetY Number of pixels to add to the Y position of $image2.
      *
      * @return EditorInterface An instance of image editor.
      */
-    public function blend(&$image1, $image2, $type='normal', $position=null, $opacity = 1);
+    public function blend(&$image1, $image2, $type='normal', $opacity = 1.0, $position = 'top-left', $offsetX = 0, $offsetY = 0 );
 
     /**
      * Compare two images and returns a hamming distance. A value of 0 indicates a likely similar picture. A value between 1 and 10 is potentially a variation. A value greater than 10 is likely a different image.
