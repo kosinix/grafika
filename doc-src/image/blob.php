45366138d730074b3696c5c6eab2f4841f3c99ae
kosinix <kosinix@users.noreply.github.com>
2016-09-27 13:37:23 +0800

    Add example

--- doc-src/image/blob.php
+++ doc-src/image/blob.php
@@ -7,6 +7,15 @@ $info = $parser->documentMethod($methodName);
 ?>
 <div id="content" class="content">
     <?php include '../parts/default-content.php'; ?>
+    <h5>Examples</h5>
+        <p>This will output a PNG image directly to the browser:</p>
+        <pre><code>use Grafika/Grafika;
+
+$image = Grafika::createImage( 'source.png' );
+
+header('Content-type: image/png'); // Tell the browser we're sending a png image
+$image->blob('PNG'); // Output raw binary png format</code></pre>
+
 </div>
 <?php include '../parts/sidebar.php'; ?>
 <?php include '../parts/bottom.php'; ?>
